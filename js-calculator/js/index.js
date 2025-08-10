d41d58d02806d0a4027ca4a7e721058a4ea6dd48
andrei-myshoves <andreimyshoves89@gmail.com>
2025-08-10 11:00:51 +0200

    add focus  input in js code

--- js-calculator/js/index.js
+++ js-calculator/js/index.js
@@ -86,6 +86,7 @@ clearButton.addEventListener("click", () => {
 
   inputElement.value = "";
   displayElement.textContent = "0";
+  inputElement.focus();
 });
 
 function handleOperation(operationName) {
@@ -103,6 +104,7 @@ function handleOperation(operationName) {
     store.operation = operationName;
     updateDisplay();
   }
+  inputElement.focus();
 }
 
 function calculateResult() {
@@ -119,6 +121,7 @@ function calculateResult() {
     inputElement.value = "";
     updateDisplay();
   }
+  inputElement.focus();
 }
 function updateDisplay() {
   if (store.operation !== null) {
