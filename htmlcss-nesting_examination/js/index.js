8ded781a675478ee86c775f483a518b64e9ec4e2
andrei-myshoves <andreimyshoves89@gmail.com>
2025-08-15 15:24:28 +0200

    add animation for logo burger-menu and add href for link

--- htmlcss-nesting_examination/js/index.js
+++ htmlcss-nesting_examination/js/index.js
@@ -1,20 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
   const burger = document.getElementById("burger-menu");
   const nav = document.getElementById("header__nav");
+  const links = document.querySelectorAll(".header__nav-link");
 
-  function handleToggleBurgerMenu() {
-    nav.classList.toggle("active");
-    burger.classList.toggle("open");
-
-    const expanded = burger.classList.contains("open");
-    burger.setAttribute("aria-expanded", expanded);
-
-    if (expanded) {
-      document.body.classList.add("no-scroll");
-    } else {
-      document.body.classList.remove("no-scroll");
-    }
+  function toggleMenu() {
+    const isOpen = burger.classList.toggle("open");
+    nav.classList.toggle("active", isOpen);
+    document.body.classList.toggle("no-scroll", isOpen);
+    burger.setAttribute("aria-expanded", isOpen);
   }
 
-  burger.addEventListener("click", handleToggleBurgerMenu);
+  burger.addEventListener("click", toggleMenu);
+
+  links.forEach((link) => {
+    link.addEventListener("click", (e) => {
+      const targetId = link.getAttribute("href");
+
+      if (targetId.startsWith("#")) {
+        e.preventDefault();
+
+        burger.classList.remove("open");
+        nav.classList.remove("active");
+        document.body.classList.remove("no-scroll");
+        burger.setAttribute("aria-expanded", false);
+
+        setTimeout(() => {
+          document.querySelector(targetId).scrollIntoView({
+            behavior: "smooth",
+          });
+        }, 300);
+      }
+    });
+  });
 });
